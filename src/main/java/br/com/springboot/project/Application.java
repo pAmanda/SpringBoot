bf450dca1d6fdf7017a63018f53badaca2fc60d5
amanda.pires <amanda.pires@mac-17.localdomain>
2018-03-01 18:01:52 -0300

    Changes.

--- src/main/java/br/com/springboot/project/Application.java
+++ src/main/java/br/com/springboot/project/Application.java
@@ -1,7 +1,13 @@
 package br.com.springboot.project;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 public class Application {
@@ -9,4 +15,20 @@ public class Application {
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
+
+  //INTERNACIONALIZATION
+  @Bean
+  public LocaleResolver localeResolver() {
+    SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+    localeResolver.setDefaultLocale(Locale.US);
+    return localeResolver;
+  }
+
+  @Bean
+  public ResourceBundleMessageSource bundleMessageSource() {
+    ResourceBundleMessageSource message = new ResourceBundleMessageSource();
+    message.setBasename("messages");
+    return message;
+  }
+
 }
--- src/main/java/br/com/springboot/project/helloworld/HelloWordController.java
+++ src/main/java/br/com/springboot/project/helloworld/HelloWordController.java
@@ -1,25 +1,33 @@
 package br.com.springboot.project.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWordController {
 
-  @GetMapping(path="/hello-word")
-  public String helloWord(){
-    return"Hello Word!";
+  @Autowired
+  public ResourceBundleMessageSource messageSource;
+
+  @GetMapping(path="/hello-world")
+  public String helloWord(@RequestHeader(name="Accept-Language", required=false) Locale locale){
+    return messageSource.getMessage("good.morning", null,  locale);
   }
 
-  @GetMapping(path="/hello-word-bean")
+  @GetMapping(path="/hello-world-bean")
   public HelloWorldBean helloWordBean(){
-    return new HelloWorldBean("Hello Word!");
+    return new HelloWorldBean("Hello World!");
   }
     
-  @GetMapping(path="/hello-word-bean/{name}")
+  @GetMapping(path="/hello-world-bean/{name}")
   public HelloWorldBean helloWordBeanPathVariable(@PathVariable String name){
-    return new HelloWorldBean(String.format("Hello Word, %s!", name));
+    return new HelloWorldBean(String.format("Hello World, %s!", name));
   }
-  
+
 }
