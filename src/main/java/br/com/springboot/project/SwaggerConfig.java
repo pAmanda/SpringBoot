09d86f713b8ff6830db1c6cbd807b83a7d9046e0
amanda.pires <amanda.pires@192.168.7.54>
2018-03-06 15:49:04 -0300

    Testing jenkins.

--- src/main/java/br/com/springboot/project/SwaggerConfig.java
+++ src/main/java/br/com/springboot/project/SwaggerConfig.java
@@ -2,7 +2,10 @@ package br.com.springboot.project;
 
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @Configuration
+@EnableSwagger2
 public class SwaggerConfig {
 
 }
